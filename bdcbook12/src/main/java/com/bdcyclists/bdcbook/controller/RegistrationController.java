a83819299371809a431fe2dc1ebf6e575d9f0552
Mozammel Haque <mozammel@livingoncodes.com>
2015-01-09 18:27:48 +0600

    Refactor Registration Form and delete a folder

---
+++ bdcbook12/src/main/java/com/bdcyclists/bdcbook/controller/RegistrationController.java
@@ -0,0 +1,37 @@
+package com.bdcyclists.bdcbook.controller;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bdcyclists.bdcbook.dto.RegistrationForm;
+
+@Controller
+public class RegistrationController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(RegistrationController.class);
+	
+	@RequestMapping(value="/user/register", method = RequestMethod.GET)
+	public String showRegistrationPage(RegistrationForm registrationForm) {
+		System.out.println("from controller");
+		LOGGER.debug("Rendering RegistrationPage.");
+		return "registration";
+	}
+	
+	
+	@RequestMapping(value="/user/register", method = RequestMethod.POST)
+	public String registerUserAccount(@Valid RegistrationForm registrationForm,
+			BindingResult result) {
+
+		if(result.hasErrors()) {
+			return "registration";
+		}
+		return "registration";
+	}
+
+}

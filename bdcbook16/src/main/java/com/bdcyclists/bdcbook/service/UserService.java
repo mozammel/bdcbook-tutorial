9a27dc82c526d4ddc1595ed6443d499434ecaf46
Mozammel Haque <mozammel@livingoncodes.com>
2015-01-10 20:21:48 +0600

    Saving RegistrationForm data

---
+++ bdcbook16/src/main/java/com/bdcyclists/bdcbook/service/UserService.java
@@ -0,0 +1,11 @@
+package com.bdcyclists.bdcbook.service;
+
+import com.bdcyclists.bdcbook.domain.User;
+import com.bdcyclists.bdcbook.dto.RegistrationForm;
+
+public interface UserService {
+
+	public User findByEmail(String email);
+
+	public User registerNewUserAccount(RegistrationForm registrationForm);
+}
---
+++ bdcbook16/src/main/java/com/bdcyclists/bdcbook/service/UserServiceImpl.java
@@ -0,0 +1,43 @@
+package com.bdcyclists.bdcbook.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bdcyclists.bdcbook.domain.User;
+import com.bdcyclists.bdcbook.dto.RegistrationForm;
+import com.bdcyclists.bdcbook.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	private UserRepository repository;
+
+	@Autowired
+	public UserServiceImpl(UserRepository repository) {
+		this.repository = repository;
+	}
+
+	@Override
+	public User findByEmail(String email) {
+		return repository.findByEmail(email);
+	}
+
+	@Override
+	public User registerNewUserAccount(RegistrationForm registrationForm) {
+		
+		User.Builder user = User.getBuilder()
+				.email(registrationForm.getEmail())
+				.firstName(registrationForm.getFirstName())
+				.lastName(registrationForm.getLastName())
+				.password(registrationForm.getPassword());
+				
+		User registered = user.build();
+		
+		
+		
+		return repository.save(registered);
+	}
+	
+	
+	
+}
